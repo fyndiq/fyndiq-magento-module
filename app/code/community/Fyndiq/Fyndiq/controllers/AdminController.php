c1e1b5d4ae02a0d7bf49aafdaa2619e4a7400a01
Håkan Nylén <hakan@fyndiq.se>
2014-09-11 10:10:46 +0200

    Clean up in the code

--- app/code/community/Fyndiq/Fyndiq/controllers/AdminController.php
+++ app/code/community/Fyndiq/Fyndiq/controllers/AdminController.php
@@ -7,6 +7,7 @@
  * Time: 09:50
  */
 require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
+
 class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
 {
 
@@ -24,6 +25,10 @@ class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
         $this->renderLayout();
     }
 
+    /**
+     * The page where everything happens.
+     * TODO: maybe move this to index instead?
+     */
     public function exportproductsAction()
     {
         $this->loadLayout(array('default'));
@@ -32,28 +37,34 @@ class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
         try {
             FmHelpers::call_api('GET', 'account/');
             $api_available = true;
-        }
-        catch (Exception $e) {
+        } catch (Exception $e) {
             $api_available = false;
             $page_args['message'] = $e->getMessage();
         }
-        if($this->getAPIToken() == "" OR $this->getUsername() == "") {
+        if ($this->getAPIToken() == "" OR $this->getUsername() == "") {
             $this->setupTemplate('fyndiq/needapiinfo.phtml');
-        }
-        else if(!$api_available) {
+        } else {
+            if (!$api_available) {
 
-            $this->setupTemplate('fyndiq/apierror.phtml', $page_args);
-        }
-        else {
-            $this->setupTemplate('fyndiq/exportproducts.phtml');
+                $this->setupTemplate('fyndiq/apierror.phtml', $page_args);
+            } else {
+                $this->setupTemplate('fyndiq/exportproducts.phtml');
+            }
         }
     }
 
-    private function setupTemplate($template, $data = null) {
+    /**
+     * Setting up the template with correct block and everything.
+     *
+     * @param $template
+     * @param null $data
+     */
+    private function setupTemplate($template, $data = null)
+    {
         $block = $this->getLayout()
             ->createBlock('Fyndiq_Fyndiq_Block_Admin', 'fyndiq.admin')
             ->setTemplate($template);
-        if($data != null) {
+        if ($data != null) {
             $block->setData('data', $data);
         }
         $this->getLayout()->getBlock('content')->append($block);
@@ -61,30 +72,23 @@ class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
         $this->renderLayout();
     }
 
+    /**
+     * Get the username from config
+     *
+     * @return mixed
+     */
     public function getUsername()
     {
         return Mage::getStoreConfig('fyndiq/fyndiq_group/username');
     }
+
+    /**
+     * Get APItoken from config
+     *
+     * @return mixed
+     */
     public function getAPIToken()
     {
         return Mage::getStoreConfig('fyndiq/fyndiq_group/apikey');
     }
-    /*
-        /**
-         * @see Mage_Core_Controller_Front_Action::renderLayout($output)
-         *
-        public function renderLayout($output = '')
-        {
-            //Add main template to to content block
-            //$block = $this->getLayout()
-            //    ->createBlock('Mage_Core_Block_Template', 'fyndiq.exportproducts')
-            //    ->setTemplate('fyndiq/exportproducts.phtml');
-            //$block->assign(get_object_vars($this));
-
-            $mainViewBlock = $this->getLayout()->addBlock(new Fyndiq_Fyndiq_Block_exportproducts(), 'Fyndiq/exportproducts')->setTemplate('fyndiq/exportproducts.phtml');
-            $mainViewBlock->assign(get_object_vars($this));
-            $this->getLayout()->getBlock('content')->append($mainViewBlock);
-
-            return parent::renderLayout($output);
-        }*/
 }
--- app/code/community/Fyndiq/Fyndiq/includes/helpers.php
+++ app/code/community/Fyndiq/Fyndiq/includes/helpers.php
@@ -8,42 +8,69 @@
 require_once(dirname(dirname(__FILE__)) . '/includes/messages.php');
 require_once(dirname(dirname(__FILE__)) . '/includes/config.php');
 require_once(dirname(dirname(__FILE__)) . '/includes/api.php');
-class FmHelpers {
 
-    public static function api_connection_exists() {
+class FmHelpers
+{
+
+    public static function api_connection_exists()
+    {
         $ret = false;
-        if(FmConfig::getBool('username') AND FmConfig::getBool('apikey')) {
+        if (FmConfig::getBool('username') AND FmConfig::getBool('apikey')) {
             $ret = true;
         }
+
         return $ret;
     }
 
-    public static function all_settings_exist() {
+    public static function all_settings_exist()
+    {
         $ret = false;
-        if(FmConfig::getBool('language') AND FmConfig::getBool('currency')) {
+        if (FmConfig::getBool('language') AND FmConfig::getBool('currency')) {
             $ret = true;
         }
+
         return $ret;
     }
 
-    ## wrappers around FyndiqAPI
-    # uses stored connection credentials for authentication
-    public static function call_api($method, $path, $data=array()) {
+    /**
+     * wrappers around FyndiqAPI
+     * uses stored connection credentials for authentication
+     *
+     * @param $method
+     * @param $path
+     * @param array $data
+     * @return array
+     */
+    public static function call_api($method, $path, $data = array())
+    {
         $username = FmConfig::get('username');
         $api_token = FmConfig::get('apikey');
 
         return FmHelpers::call_api_raw($username, $api_token, $method, $path, $data);
     }
 
-    # add descriptive error messages for common errors, and re throw same exception
-    public static function call_api_raw($username, $api_token, $method, $path, $data=array()) {
-        $module = "FyndiqMechantMagento".FmConfig::getVersion();
+    /**
+     * add descriptive error messages for common errors, and re throw same exception
+     *
+     * @param $username
+     * @param $api_token
+     * @param $method
+     * @param $path
+     * @param array $data
+     * @return array
+     * @throws FyndiqAPITooManyRequests
+     * @throws FyndiqAPIConnectionFailed
+     * @throws FyndiqAPIAuthorizationFailed
+     */
+    public static function call_api_raw($username, $api_token, $method, $path, $data = array())
+    {
+        $module = "FyndiqMechantMagento" . FmConfig::getVersion();
 
         try {
             return FyndiqAPI::call($module, $username, $api_token, $method, $path, $data);
 
         } catch (FyndiqAPIConnectionFailed $e) {
-            throw new FyndiqAPIConnectionFailed(FmMessages::get('api-network-error').': '.$e->getMessage());
+            throw new FyndiqAPIConnectionFailed(FmMessages::get('api-network-error') . ': ' . $e->getMessage());
 
         } catch (FyndiqAPIAuthorizationFailed $e) {
             throw new FyndiqAPIAuthorizationFailed(FmMessages::get('api-incorrect-credentials'));
@@ -51,13 +78,5 @@ class FmHelpers {
         } catch (FyndiqAPITooManyRequests $e) {
             throw new FyndiqAPITooManyRequests(FmMessages::get('api-too-many-requests'));
         }
-    }
-
-    public static function db_escape($value) {
-
-    }
-
-    public static function get_shop_url($context) {
-
     }
 }
