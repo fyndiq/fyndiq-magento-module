4550c5162dc1a86e3f1af98f565300968bd31b9c
Håkan Nylén <hakan@fyndiq.se>
2014-09-15 10:09:41 +0200

    Creating a order table

---
+++ app/code/community/Fyndiq/Fyndiq/Model/Order.php
@@ -0,0 +1,10 @@
+<?php
+class Fyndiq_Fyndiq_Model_Order extends Mage_Core_Model_Abstract
+{
+
+    public function _construct()
+    {
+        parent::_construct();
+        $this->_init('fyndiq/order');
+    }
+}
---
+++ app/code/community/Fyndiq/Fyndiq/sql/Fyndiq_Fyndiq_setup/install-1.0.0.php
@@ -0,0 +1,23 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$table = $installer->getConnection()
+    ->newTable($installer->getTable('fyndiq/order'))
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'identity'  => true,
+            'unsigned'  => true,
+            'nullable'  => false,
+            'primary'   => true,
+        ), 'Id')
+    ->addColumn('fyndiq_orderid', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'nullable'  => false,
+        ), 'Fyndiq Order')
+    ->addColumn('order_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'nullable'  => false,
+        ), 'Magento Order');
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
