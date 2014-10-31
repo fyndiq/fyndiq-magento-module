72238dff538c1617a51b17681fdb59d47bfef422
Håkan Nylén <hakan@fyndiq.se>
2014-10-31 15:13:07 +0100

    using internal qty for orders instead

--- app/code/community/Fyndiq/Fyndiq/sql/Fyndiq_Fyndiq_setup/install-1.0.0.php
+++ app/code/community/Fyndiq/Fyndiq/sql/Fyndiq_Fyndiq_setup/install-1.0.0.php
@@ -17,10 +17,7 @@ $table = $installer->getConnection()
         ), 'Magento Order')
     ->addColumn('fyndiq_orderid', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
             'nullable'  => false,
-        ), 'Fyndiq Order')
-    ->addColumn('fyndiq_stock', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-            'nullable'  => false,
-        ), 'amount of stock been sent to Fyndiq last time.');
+        ), 'Fyndiq Order');
 $installer->getConnection()->createTable($table);
 
 $installer->endSetup();
