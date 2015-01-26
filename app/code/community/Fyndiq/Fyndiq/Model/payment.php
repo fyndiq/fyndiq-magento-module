a9403bc3791f2d870358509af1268fb54e1ffa9d
Håkan Nylén <hakan@fyndiq.se>
2015-01-26 09:57:47 +0100

    Make so Fyndiq won't show up on checkout
    as payment method.

--- app/code/community/Fyndiq/Fyndiq/Model/payment.php
+++ app/code/community/Fyndiq/Fyndiq/Model/payment.php
@@ -7,5 +7,6 @@ class Fyndiq_Fyndiq_Model_Payment extends Mage_Payment_Model_Method_Abstract
     protected $_isInitializeNeeded      = true;
     protected $_canUseInternal          = false;
     protected $_canUseForMultishipping  = false;
+    protected $_canUseCheckout = false;
 
 }
