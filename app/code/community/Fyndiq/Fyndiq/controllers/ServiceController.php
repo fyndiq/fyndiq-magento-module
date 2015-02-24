58450886d46b656502c69e091281beed783e0f38
Håkan Nylén <hakan@fyndiq.se>
2015-02-24 14:28:41 +0100

    don't save lastupdated_date if error occur

--- app/code/community/Fyndiq/Fyndiq/controllers/ServiceController.php
+++ app/code/community/Fyndiq/Fyndiq/controllers/ServiceController.php
@@ -425,15 +425,15 @@ class Fyndiq_Fyndiq_ServiceController extends Mage_Adminhtml_Controller_Action
 
             //Updating or adding the date setting
             $newdate = date("Y-m-d H:i:s");
-            if ($settingexists) {
-                Mage::getModel('fyndiq/setting')->updateSetting("order_lastdate", $newdate);
-            } else {
-                Mage::getModel('fyndiq/setting')->saveSetting("order_lastdate", $newdate);
-            }
             foreach ($ret["data"] as $order) {
                 if (!Mage::getModel('fyndiq/order')->orderExists($order->id)) {
                     Mage::getModel('fyndiq/order')->create($order);
                 }
+            }
+            if ($settingexists) {
+                Mage::getModel('fyndiq/setting')->updateSetting("order_lastdate", $newdate);
+            } else {
+                Mage::getModel('fyndiq/setting')->saveSetting("order_lastdate", $newdate);
             }
             self::response($ret);
         } catch (Exception $e) {
