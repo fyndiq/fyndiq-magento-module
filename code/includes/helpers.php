1282b87d59afc8c2f3ad33fb763bbd4c7ef85216
aquilax <aquilax@gmail.com>
2015-03-20 13:51:10 +0100

    Switched to FyndiqAPICaller; closes #35

--- code/includes/helpers.php
+++ code/includes/helpers.php
@@ -9,44 +9,25 @@
 require_once(dirname(dirname(__FILE__)) . '/includes/messages.php');
 require_once(dirname(dirname(__FILE__)) . '/includes/config.php');
 require_once(dirname(dirname(__FILE__)) . '/includes/api/fyndiqAPI.php');
-
-class FyndiqAPIDataInvalid extends Exception{}
-
-class FyndiqAPIConnectionFailed extends Exception{}
-
-class FyndiqAPIPageNotFound extends Exception{}
-
-class FyndiqAPIAuthorizationFailed extends Exception{}
-
-class FyndiqAPITooManyRequests extends Exception{}
-
-class FyndiqAPIServerError extends Exception{}
-
-class FyndiqAPIBadRequest extends Exception{}
-
-class FyndiqAPIUnsupportedStatus extends Exception{}
+require_once(MAGENTO_ROOT . '/fyndiq/shared/src/FyndiqAPICall.php');
 
 class FmHelpers
 {
 
     public static function api_connection_exists()
     {
-        $ret = false;
-        if (FmConfig::getBool('username') AND FmConfig::getBool('apikey')) {
-            $ret = true;
+        if (FmConfig::getBool('username') && FmConfig::getBool('apikey')) {
+            return true;
         }
-
-        return $ret;
+        return false;
     }
 
     public static function all_settings_exist()
     {
-        $ret = false;
-        if (FmConfig::getBool('language') AND FmConfig::getBool('currency')) {
-            $ret = true;
+        if (FmConfig::getBool('language') && FmConfig::getBool('currency')) {
+            return true;
         }
-
-        return $ret;
+        return false;
     }
 
     /**
@@ -56,92 +37,15 @@ class FmHelpers
      * @param $method
      * @param $path
      * @param array $data
-     * @param string $filename
-     * @return array
+     * @return mixed
      */
     public static function call_api($method, $path, $data = array())
     {
         $username = FmConfig::get('username');
-        $api_token = FmConfig::get('apikey');
+        $apiToken = FmConfig::get('apikey');
+        $userAgent = "FyndiqMechantMagento" . FmConfig::getVersion();
 
-        return FmHelpers::call_api_raw($username, $api_token, $method, $path, $data);
-    }
-
-    /**
-     * add descriptive error messages for common errors, and re throw same exception
-     *
-     * @param $username
-     * @param $api_token
-     * @param $method
-     * @param $path
-     * @param array $data
-     * @throws FyndiqAPIUnsupportedStatus
-     * @throws FyndiqAPIPageNotFound
-     * @throws FyndiqAPIServerError
-     * @throws FyndiqAPIBadRequest
-     * @throws FyndiqAPITooManyRequests
-     * @throws FyndiqAPIAuthorizationFailed
-     * @throws FyndiqAPIDataInvalid
-     * @return array
-     */
-    public static function call_api_raw($username, $api_token, $method, $path, $data = array())
-    {
-        $module = "FyndiqMechantMagento" . FmConfig::getVersion();
-
-        $response = FyndiqAPI::call($module, $username, $api_token, $method, $path, $data);
-
-
-        if ($response['status'] == 404) {
-            throw new FyndiqAPIPageNotFound('Not Found: ' . $path);
-        }
-
-        if ($response['status'] == 401) {
-            throw new FyndiqAPIAuthorizationFailed('Unauthorized');
-        }
-
-        if ($response['status'] == 429) {
-            throw new FyndiqAPITooManyRequests('Too Many Requests');
-        }
-
-        if ($response['status'] == 500) {
-            throw new FyndiqAPIServerError('Server Error');
-        }
-
-        // if json_decode failed
-        if (json_last_error() != JSON_ERROR_NONE) {
-            throw new FyndiqAPIDataInvalid('Error in response data');
-        }
-
-        // 400 may contain error messages intended for the user
-        if ($response['status'] == 400) {
-            $message = '';
-
-            // if there are any error messages, save them to class static member
-            if (property_exists($response["data"], 'error_messages')) {
-                $error_messages = $response["data"]->error_messages;
-
-                // if it contains several messages as an array
-                if (is_array($error_messages)) {
-
-                    foreach ($response["data"]->error_messages as $error_message) {
-                        self::$error_messages[] = $error_message;
-                    }
-
-                    // if it contains just one message as a string
-                } else {
-                    self::$error_messages[] = $error_messages;
-                }
-            }
-
-            throw new FyndiqAPIBadRequest('Bad Request');
-        }
-
-        $success_http_statuses = array('200', '201');
-
-        if (!in_array($response['status'], $success_http_statuses)) {
-            throw new FyndiqAPIUnsupportedStatus('Unsupported HTTP status: ' . $response['status']);
-        }
-
-        return $response;
+        return FyndiqAPICall::callApiRaw($userAgent, $username, $apiToken, $method, $path, $data,
+            array('FyndiqAPI', 'call'));
     }
 }
