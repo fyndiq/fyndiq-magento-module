524ccd879fe0a877936a5f723a70b7082bf5d8c8
aquilax <aquilax@gmail.com>
2015-03-20 12:46:26 +0100

    Fixes for shared

--- code/Block/Admin.php
+++ code/Block/Admin.php
@@ -19,6 +19,26 @@ class Fyndiq_Fyndiq_Block_Admin extends Mage_Core_Block_Template
     }
 
     /**
+     * Get shared path
+     *
+     * @return string
+     */
+    public function getSharedPath()
+    {
+        return Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB) . 'fyndiq/shared/';
+    }
+
+    /**
+     * Get service path
+     *
+     * @return string
+     */
+    public function getServicePath()
+    {
+        return $this->getAdminPath('fyndiq/service') . '?isAjax=true';
+    }
+
+    /**
      * Get admin path
      *
      * @param $path
