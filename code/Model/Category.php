f65562921bbf6ebfae6ad2a36833bb24fc821429
aquilax <aquilax@gmail.com>
2015-03-30 14:18:17 +0200

    Optimized a little

--- code/Model/Category.php
+++ code/Model/Category.php
@@ -6,7 +6,8 @@ class FmCategory {
         $category = Mage::getModel('catalog/category');
         $rootCategoryId = Mage::app()->getStore($storeId)->getRootCategoryId();
         $data = array();
-        if($category_id === 0) {
+        $getRootOnly = $category_id === 0;
+        if($getRootOnly) {
             $categories = $category->getCollection()
                 ->addAttributeToSelect('*')
                 ->setStoreId($storeId)
@@ -14,26 +15,7 @@ class FmCategory {
                 ->addAttributeToFilter('is_active', '1')
                 ->addAttributeToFilter('include_in_menu', '1')
                 ->addAttributeToSort('position', 'asc')->getItems();
-
-            $parentlvl = false;
-            foreach($categories as $cat) {
-                if($parentlvl == false) {
-                    $parentlvl = $cat->getLevel();
-                }
-                if($parentlvl != false && $cat->getLevel() != $parentlvl) {
-                    continue;
-                }
-                $categoryData = array(
-                    'id' => $cat->getId(),
-                    'url' => $cat->getUrl(),
-                    'name' => $cat->getName(),
-                    'image' => $cat->getImageUrl(),
-                    'isActive' => $cat->getIsActive()
-                );
-                array_push($data, $categoryData);
-            }
-        }
-        else {
+        } else {
             $categories = $category->getCollection()
                 ->addAttributeToSelect('*')
                 ->setStoreId($storeId)
@@ -41,17 +23,25 @@ class FmCategory {
                 ->addAttributeToFilter('is_active', '1')
                 ->addAttributeToFilter('parent_id', array('eq' => $category_id))
                 ->addAttributeToSort('position', 'asc')->getItems();
-
-            foreach ($categories as $cat) {
-                $categoryData = array(
-                    'id' => $cat->getId(),
-                    'url' => $cat->getUrl(),
-                    'name' => $cat->getName(),
-                    'image' => $cat->getImageUrl(),
-                    'isActive' => $cat->getIsActive()
-                );
-                array_push($data, $categoryData);
+        }
+        $parentLvl = false;
+        foreach($categories as $cat) {
+            if ($getRootOnly) {
+                if($parentLvl == false) {
+                    $parentLvl = $cat->getLevel();
+                }
+                if($parentLvl != false && $cat->getLevel() != $parentLvl) {
+                    continue;
+                }
             }
+            $categoryData = array(
+                'id' => $cat->getId(),
+                'url' => $cat->getUrl(),
+                'name' => $cat->getName(),
+                'image' => $cat->getImageUrl(),
+                'isActive' => $cat->getIsActive()
+            );
+            array_push($data, $categoryData);
         }
         return $data;
     }
