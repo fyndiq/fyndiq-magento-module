126b46517e5cc05b01d2c0954adca6594c4ee1ba
Håkan Nylén <hakan@fyndiq.se>
2015-04-08 09:51:22 +0200

    fixed working notification controller

--- code/controllers/NotificationController.php
+++ code/controllers/NotificationController.php
@@ -7,12 +7,17 @@ require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
 class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Action
 {
     function indexAction() {
-        $orderid = $_GET['orderid'];
+        $orderid = $_GET['order_id'];
 
         $storeId = Mage::app()->getStore()->getStoreId();
-        $ret = FmHelpers::call_api($storeId, 'GET', 'orders/'.intval($orderid));
+        $ret = FmHelpers::call_api($storeId, 'GET', 'orders/'.intval($orderid) . '/');
 
-        var_dump($ret);
-        $this->getResponse()->setBody(true);
+        $fyndiq_order = $ret['data'];
+
+        $order_model = Mage::getModel('fyndiq/order');
+
+        if (!$order_model->orderExists($fyndiq_order->id)) {
+            $order_model->create($storeId, $fyndiq_order);
+        }
     }
 }
