a3e394656f271e54b40a59b9c0402f2ba593be42
Håkan Nylén <hakan@fyndiq.se>
2015-04-08 10:34:55 +0200

    added check

--- code/controllers/NotificationController.php
+++ code/controllers/NotificationController.php
@@ -5,18 +5,22 @@ require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
 
 class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Action
 {
-    function indexAction() {
-        $orderid = $_GET['order_id'];
+    function indexAction()
+    {
+        $orderid = intval($_GET['order_id']);
 
-        $storeId = Mage::app()->getStore()->getStoreId();
-        $ret = FmHelpers::call_api($storeId, 'GET', 'orders/'.intval($orderid) . '/');
+        if ($orderid > 0) {
 
-        $fyndiq_order = $ret['data'];
+            $storeId = Mage::app()->getStore()->getStoreId();
+            $ret = FmHelpers::call_api($storeId, 'GET', 'orders/' . $orderid . '/');
 
-        $order_model = Mage::getModel('fyndiq/order');
+            $fyndiq_order = $ret['data'];
 
-        if (!$order_model->orderExists($fyndiq_order->id)) {
-            $order_model->create($storeId, $fyndiq_order);
+            $order_model = Mage::getModel('fyndiq/order');
+
+            if (!$order_model->orderExists($fyndiq_order->id)) {
+                $order_model->create($storeId, $fyndiq_order);
+            }
         }
     }
 }
