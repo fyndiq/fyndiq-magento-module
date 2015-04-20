8514ca0e2273dc4f707048e62c1e477f2b6e9f9c
aquilax <aquilax@gmail.com>
2015-04-20 09:52:36 +0200

    Extend notification to handle multiple events

--- code/controllers/NotificationController.php
+++ code/controllers/NotificationController.php
@@ -7,6 +7,24 @@ class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Ac
 {
     function indexAction()
     {
+        $event = $this->getRequest()->getParam('event');
+        $eventName = $event ? $event : false;
+        if ($eventName) {
+            if (method_exists($this, $eventName)) {
+                return $this->$eventName();
+            }
+        }
+        header('HTTP/1.0 400 Bad Request');
+        die('400 Bad Request');
+    }
+
+    /**
+     * Handle new order
+     *
+     * @return bool
+     */
+    function order_created()
+    {
         $orderId = $this->getRequest()->getParam('order_id');
         $orderId = is_numeric($orderId) ? intval($orderId) : 0;
         if ($orderId > 0) {
@@ -23,9 +41,22 @@ class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Ac
                 }
             } catch (Exception $e) {
                 header('HTTP/1.0 500 Internal Server Error');
+                die('500 Internal Server Error');
             }
             return true;
         }
         header('HTTP/1.0 400 Bad Request');
+        die('400 Bad Request');
     }
+
+
+    /**
+     * Generate feed
+     *
+     */
+    private function ping() {
+        $token = $this->getRequest()->getParam('token');
+        die($token);
+    }
+
 }
