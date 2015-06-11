d2a76a0f3147cdbf75b1b1da8dcfda9312a4d51f
Håkan Nylén <hakan@fyndiq.se>
2015-06-11 16:43:06 +0200

    moved required to constructor to work with tests

--- code/controllers/AdminController.php
+++ code/controllers/AdminController.php
@@ -6,8 +6,6 @@
  * Date: 18/08/14
  * Time: 09:50
  */
-require_once(MAGENTO_ROOT . '/fyndiq/shared/src/init.php');
-require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
 
 class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
 {
@@ -16,6 +14,8 @@ class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
 
     protected function _construct()
     {
+        require_once(MAGENTO_ROOT . '/fyndiq/shared/src/init.php');
+        require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
         FyndiqTranslation::init(Mage::app()->getLocale()->getLocaleCode());
     }
 
@@ -26,7 +26,7 @@ class Fyndiq_Fyndiq_AdminController extends Mage_Adminhtml_Controller_Action
     {
         $this->loadLayout(array('default'));
 
-        $this->setTemplate('fyndiq/exportproducts.phtml');
+        return $this->setTemplate('fyndiq/exportproducts.phtml');
     }
 
 
