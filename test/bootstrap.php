7ba6f794b5f8fc0ba97f048246a33817eb3091a6
Håkan Nylén <hakan@fyndiq.se>
2015-06-11 16:42:12 +0200

    require the important files for the phpunit tests

---
+++ test/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+require_once(dirname(__FILE__) . '/dep/mage_adminhtml_controller.php');
+require_once(dirname(__FILE__) . '/dep/Mage.php');
+require_once(dirname(dirname(__FILE__)) . '/code/controllers/AdminController.php');
