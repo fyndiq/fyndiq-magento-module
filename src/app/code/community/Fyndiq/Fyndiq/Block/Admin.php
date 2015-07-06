1f464bcebc0a0069e5250e566053a6523f2c1676
Håkan Nylén <hakan@fyndiq.se>
2015-07-06 10:58:11 +0200

    added commit to version

--- src/app/code/community/Fyndiq/Fyndiq/Block/Admin.php
+++ src/app/code/community/Fyndiq/Fyndiq/Block/Admin.php
@@ -7,6 +7,7 @@ require_once(dirname(dirname(__FILE__)) . '/includes/config.php');
  */
 class Fyndiq_Fyndiq_Block_Admin extends Mage_Core_Block_Template
 {
+    const COMMIT = 'XXXXXX';
 
     /**
      * Get frontend path
@@ -97,7 +98,7 @@ class Fyndiq_Fyndiq_Block_Admin extends Mage_Core_Block_Template
 
     public function getVersion()
     {
-        return FmConfig::getVersion();
+        return FmConfig::getVersion() . " " . self::COMMIT;
     }
 
     public function getLastUpdatedDate($storeId)
