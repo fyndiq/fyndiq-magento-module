7acd6d5aa07159cf9fb0add79eb3aa3e5a78cd22
Håkan Nylén <hakan@fyndiq.se>
2015-07-07 10:39:28 +0200

    updated useragent

--- src/app/code/community/Fyndiq/Fyndiq/includes/helpers.php
+++ src/app/code/community/Fyndiq/Fyndiq/includes/helpers.php
@@ -36,7 +36,7 @@ class FmHelpers
     {
         $username = FmConfig::get('username', $storeId);
         $apiToken = FmConfig::get('apikey', $storeId);
-        $userAgent = "FyndiqMerchantMagento" . FmConfig::getVersion() . "-" . Mage::getVersion();
+        $userAgent = FyndiqUtils::getUserAgentString("Magento", Mage::getVersion(), "FyndiqMerchant", FmConfig::getVersion(), FmConfig::COMMIT);
 
         return FyndiqAPICall::callApiRaw(
             $userAgent,
