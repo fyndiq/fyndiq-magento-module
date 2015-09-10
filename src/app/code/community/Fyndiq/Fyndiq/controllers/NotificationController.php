b9d7e2e02510fb4e58b86761a50db879295a88d5
aquilax <aquilax@gmail.com>
2015-09-10 12:01:19 +0200

    Init translations in notifications

--- src/app/code/community/Fyndiq/Fyndiq/controllers/NotificationController.php
+++ src/app/code/community/Fyndiq/Fyndiq/controllers/NotificationController.php
@@ -4,10 +4,16 @@ require_once(dirname(dirname(__FILE__)) . '/Model/Order.php');
 require_once(dirname(dirname(__FILE__)) . '/includes/config.php');
 require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
 require_once(dirname(dirname(__FILE__)) . '/Model/Product_info.php');
-require_once(MAGENTO_ROOT . '/fyndiq/shared/src/FyndiqUtils.php');
+require_once(MAGENTO_ROOT . '/fyndiq/shared/src/init.php');
 
 class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Action
 {
+
+    public function __construct()
+    {
+        FyndiqTranslation::init(Mage::app()->getLocale()->getLocaleCode());
+    }
+
     public function indexAction()
     {
         $event = $this->getRequest()->getParam('event');
