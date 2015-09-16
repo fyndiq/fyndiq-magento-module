f127dd5edcf432d6df50c9824a928a7b1b2b9989
Håkan Nylén <hakan@fyndiq.se>
2015-09-16 09:11:52 +0200

    added attribute group and price attribute

--- src/app/code/community/Fyndiq/Fyndiq/sql/fyndiqmodule_setup/upgrade-1.0.0-1.1.0.php
+++ src/app/code/community/Fyndiq/Fyndiq/sql/fyndiqmodule_setup/upgrade-1.0.0-1.1.0.php
@@ -4,8 +4,17 @@ $installer2 = Mage::getResourceModel('catalog/setup', 'catalog_setup');
 
 $installer2->startSetup();
 
-$attrCode = 'fyndiq_exported';
+
+// Add new Attribute group
 $attrGroupName = 'Fyndiq';
+$entityTypeId = $installer2->getEntityTypeId('catalog_product');
+$attributeSetId = $installer2->getDefaultAttributeSetId($entityTypeId);
+$installer2->addAttributeGroup($entityTypeId, $attributeSetId, $attrGroupName, 100);
+$attributeGroupId = $installer2->getAttributeGroupId($entityTypeId, $attributeSetId, $attrGroupName);
+
+
+$attrCode = 'fyndiq_exported';
+
 $attrLabel = 'Fyndiq Exported';
 $attrNote = 'Show if product is exported or not';
 
@@ -16,25 +25,46 @@ $installer2->addAttribute(
     'type'          => 'int',
     'input'         => 'select',
     'label'         => $attrLabel,
+    'group'         => $attrGroupName,
     'required'      => false,
     'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
     'searchable'    => true,
     'source'        => 'fyndiq/attribute_exported',
     'filterable_in_search' => true,
-    'sort_order'    => 5, // Place just below SKU (4)
+    'sort_order'    => 1, // Place just below SKU (4)
     'default'       => '0'
     )
 );
 
-$attrData = array(
-    $attrCode => '0',
+$attrCode2 = 'fyndiq_price';
+$attrLabel2 = 'Fyndiq Price';
+
+$installer2->addAttribute(
+    'catalog_product',
+    $attrCode2,
+    array(
+    'type'          => 'int',
+    'input'         => 'price',
+    'label'         => $attrLabel2,
+    'group'         => $attrGroupName,
+    'required'      => false,
+    'global'        => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_STORE,
+    'searchable'    => true,
+    'filterable_in_search' => true,
+    'sort_order'    => 2, // Place just below SKU (4)
+    'default'       => '0'
+    )
 );
-$storeId = 0;
-$productIds = Mage::getModel('catalog/product')->getCollection()->getAllIds();
-Mage::getModel("catalog/product_action")->updateAttributes(
-    $productIds,
-    $attrData,
-    $storeId
-);
+
+$productCollection = Mage::getModel('catalog/product')->getCollection();
+
+foreach($productCollection as $product)
+{
+    $product = Mage::getModel('catalog/product')
+                   ->load($product->getEntityId());
+    $product->setData($attrCode, 0)
+            ->getResource()
+            ->saveAttribute($product, $attrCode);
+}
 
 $installer2->endSetup();
