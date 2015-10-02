c8630128f7d78875263ba861a5b6ef0f79c7f584
aquilax <aquilax@gmail.com>
2015-10-02 08:28:32 +0200

    Added basic Swedish regions and tests

--- test/src/testFyndiqRegionHelper.php
+++ test/src/testFyndiqRegionHelper.php
@@ -3,42 +3,44 @@
 class FyndiqRegionHelperTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testCodeToRegionCodeDeProvider()
+    public function testCodeToRegionCodeProvider()
     {
         return array(
-            array('0233', 'BER'),
-            array('97888', 'BAY'),
-            array('97896', 'BAW'),
-            array('01001', 'SAS'),
-            array('80337', 'BAY'),
+            array('0233', 'BER', FyndiqRegionHelper::CODE_DE),
+            array('97888', 'BAY', FyndiqRegionHelper::CODE_DE),
+            array('97896', 'BAW', FyndiqRegionHelper::CODE_DE),
+            array('01001', 'SAS', FyndiqRegionHelper::CODE_DE),
+            array('80337', 'BAY', FyndiqRegionHelper::CODE_DE),
+            array('23332', '2xxxx', FyndiqRegionHelper::CODE_SE),
         );
     }
 
     /**
-     * @dataProvider testCodeToRegionCodeDeProvider
+     * @dataProvider testCodeToRegionCodeProvider
      */
-    public function testCodeToRegionCodeDe($code, $expected)
+    public function testCodeToRegionCode($code, $expected, $countryCode)
     {
-        $result = FyndiqRegionHelper::codeToRegionCodeDe($code);
+        $result = FyndiqRegionHelper::codeToRegionCode($code, $countryCode);
         $this->assertEquals($expected, $result);
     }
 
     public function testGetRegionNameProvider()
     {
         return array(
-            array('BER', 'Berlin'),
-            array('BAY', 'Bayern'),
-            array('SAS', 'Sachsen'),
-            array('MEC', 'Mecklenburg-Vorpommern'),
+            array('BER', 'Berlin', FyndiqRegionHelper::CODE_DE),
+            array('BAY', 'Bayern', FyndiqRegionHelper::CODE_DE),
+            array('SAS', 'Sachsen', FyndiqRegionHelper::CODE_DE),
+            array('MEC', 'Mecklenburg-Vorpommern', FyndiqRegionHelper::CODE_DE),
+            array('2xxxx', 'Skåne', FyndiqRegionHelper::CODE_SE),
         );
     }
 
     /**
      * @dataProvider testGetRegionNameProvider
      */
-    public function testGetRegionName($code, $expected)
+    public function testGetRegionName($code, $expected, $countryCode)
     {
-        $result = FyndiqRegionHelper::getRegionName($code);
+        $result = FyndiqRegionHelper::getRegionName($code, $countryCode);
         $this->assertEquals($expected, $result);
     }
 }
