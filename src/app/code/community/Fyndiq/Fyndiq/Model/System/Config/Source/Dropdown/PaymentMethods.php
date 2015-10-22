b80b643e7d6c371885bc243f014493cc1b47ad65
aquilax <aquilax@gmail.com>
2015-10-22 08:27:21 +0200

    Style

--- src/app/code/community/Fyndiq/Fyndiq/Model/System/Config/Source/Dropdown/PaymentMethods.php
+++ src/app/code/community/Fyndiq/Fyndiq/Model/System/Config/Source/Dropdown/PaymentMethods.php
@@ -4,10 +4,10 @@ class Fyndiq_Fyndiq_Model_System_Config_Source_Dropdown_PaymentMethods
 {
     public function toOptionArray()
     {
-       $methods = array();
-       $payments = Mage::getSingleton('payment/config')->getActiveMethods();
-       foreach ($payments as $paymentCode => $paymentModel) {
-            $paymentTitle = Mage::getStoreConfig('payment/'.$paymentCode.'/title');
+        $methods = array();
+        $payments = Mage::getSingleton('payment/config')->getActiveMethods();
+        foreach ($payments as $paymentCode => $paymentModel) {
+            $paymentTitle = Mage::getStoreConfig('payment/' . $paymentCode . '/title');
             $methods[$paymentCode] = array(
                 'label'   => $paymentTitle,
                 'value' => $paymentCode,
