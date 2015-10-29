7ea04a96a334ef9f223c0196e02151de4a413557
aquilax <aquilax@gmail.com>
2015-10-29 10:36:19 +0100

    Name as constant

--- src/app/code/community/Fyndiq/Fyndiq/Model/Sales/Quote/Address.php
+++ src/app/code/community/Fyndiq/Fyndiq/Model/Sales/Quote/Address.php
@@ -2,6 +2,8 @@
 
 class Fyndiq_Fyndiq_Model_Sales_Quote_Address extends Mage_Sales_Model_Quote_Address
 {
+    const CARRIER_NAME = 'fyndiq_fyndiq';
+
     public function getShippingRatesCollection()
     {
         parent::getShippingRatesCollection();
@@ -9,7 +11,7 @@ class Fyndiq_Fyndiq_Model_Sales_Quote_Address extends Mage_Sales_Model_Quote_Add
         $removeRates = array();
 
         foreach ($this->_rates as $key => $rate) {
-            if ($rate->getCarrier() == 'fyndiq_fyndiq') {
+            if ($rate->getCarrier() == self::CARRIER_NAME) {
                 $removeRates[] = $key;
             }
         }
