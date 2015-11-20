44e91e2e79b570a0345502fd49bceae922bd1807
aquilax <aquilax@gmail.com>
2015-11-20 09:34:14 +0100

    Remove the extra columns for order

--- src/app/code/community/Fyndiq/Fyndiq/sql/fyndiqmodule_setup/upgrade-1.0.0-1.0.8.php
+++ src/app/code/community/Fyndiq/Fyndiq/sql/fyndiqmodule_setup/upgrade-1.0.0-1.0.8.php
@@ -14,25 +14,4 @@ $connection->addColumn(
     )
 );
 
-$connection->addColumn(
-    $installer->getTable('fyndiq/order'),
-    'status',
-    array(
-        'type' => Varien_Db_Ddl_Table::TYPE_INTEGER,
-        'nullable' => false,
-        'default' => 0,
-        'comment' => 'Order status'
-    )
-);
-
-$connection->addColumn($installer->getTable('fyndiq/order'),
-    'data',
-    array(
-        'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
-        'nullable' => false,
-        'default' => '',
-        'comment' => 'Order payload'
-    )
-);
-
 $installer->endSetup();
