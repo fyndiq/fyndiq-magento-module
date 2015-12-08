6686ec4255dc9ad5b818449291aad6ef7537013a
Håkan Nylén <hakan@fyndiq.se>
2015-12-08 11:46:53 +0100

    adding space

--- src/app/code/community/Fyndiq/Fyndiq/sql/fyndiqmodule_setup/upgrade-1.0.0-1.1.0.php
+++ src/app/code/community/Fyndiq/Fyndiq/sql/fyndiqmodule_setup/upgrade-1.0.0-1.1.0.php
@@ -40,7 +40,7 @@ $productIds = Mage::getResourceModel('catalog/product_collection')
     ->getAllIds();
 
 //Now create an array of attribute_code => values
-$attributeData = array($attrCode =>0);
+$attributeData = array($attrCode => 0);
 
 //Set the store to affect. I used admin to change all default values
 $storeId = Mage_Core_Model_App::ADMIN_STORE_ID;
