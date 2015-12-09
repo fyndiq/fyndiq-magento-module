21e7c0ad1d3cd1e814e2e6bb92f0e5aa45430eaa
aquilax <aquilax@gmail.com>
2015-12-09 08:38:44 +0100

    Fixed controllers

--- src/app/code/community/Fyndiq/Fyndiq/controllers/NotificationController.php
+++ src/app/code/community/Fyndiq/Fyndiq/controllers/NotificationController.php
@@ -6,7 +6,7 @@ require_once(dirname(dirname(__FILE__)) . '/includes/helpers.php');
 require_once(dirname(dirname(__FILE__)) . '/Model/Product_info.php');
 require_once(MAGENTO_ROOT . '/fyndiq/shared/src/init.php');
 
-class Fyndiq_Fyndiq_Adminhtml_NotificationController extends Mage_Core_Controller_Front_Action
+class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Action
 {
 
     private $fyndiqOutput = null;
