2eb6b2c7979f3b9c88ef0fe9de56da8ccb178210
aquilax <aquilax@gmail.com>
2015-12-28 10:36:17 +0100

    Removed debug

--- src/app/code/community/Fyndiq/Fyndiq/Block/Adminhtml/Sales/Order/View.php
+++ src/app/code/community/Fyndiq/Fyndiq/Block/Adminhtml/Sales/Order/View.php
@@ -6,8 +6,6 @@ class Fyndiq_Fyndiq_Block_Adminhtml_Sales_Order_View extends Mage_Adminhtml_Bloc
     {
         parent::__construct();
 
-        error_log($this->getRequest()->getParam('order_id'));
-
         if ($this->isFyndiqOrder($this->getRequest()->getParam('order_id'))) {
             $this->_addButton('fyndiq_delivery_note', array(
                 'label' => Mage::helper('fyndiq_fyndiq')->__('Fyndiq Delivery Note'),
