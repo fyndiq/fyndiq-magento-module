50dbd3959bff54e1a5a2edee1da1f5249c44237d
aquilax <aquilax@gmail.com>
2016-01-07 11:12:12 +0100

    Better

--- src/app/code/community/Fyndiq/Fyndiq/Block/Updated.php
+++ src/app/code/community/Fyndiq/Fyndiq/Block/Updated.php
@@ -1,24 +1,12 @@
 <?php
 
-class Fyndiq_Fyndiq_Block_Updated extends Mage_Adminhtml_Block_System_Config_Form_Field
+class Fyndiq_Fyndiq_Block_Adminhtml_Updated extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
         $this->setElement($element);
-        $result = array();
-        $modules = (array)Mage::getConfig()->getNode('modules')->children();
-        foreach ($modules as $name => $module) {
-            $result[] = implode(
-                "\t",
-                array(
-                    $name,
-                    $module->codePool,
-                    $module->version,
-                    $module->active == 'true' ? 'active' : '-'
-                )
-            );
-        }
-        $html = '<textarea readonly="readonly">'.implode("\n", $result).'</textarea>';
+
+        $html = '<textarea readonly="readonly">'.''.'</textarea>';
         return $html;
     }
 }
