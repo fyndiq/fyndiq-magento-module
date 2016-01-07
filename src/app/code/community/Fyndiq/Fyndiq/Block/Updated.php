f365f5efb4e50f164af8fc46a398fce37f55a21d
aquilax <aquilax@gmail.com>
2016-01-07 11:58:05 +0100

    Label is showing

--- src/app/code/community/Fyndiq/Fyndiq/Block/Updated.php
+++ src/app/code/community/Fyndiq/Fyndiq/Block/Updated.php
@@ -1,12 +1,19 @@
 <?php
 
-class Fyndiq_Fyndiq_Block_Adminhtml_Updated extends Mage_Adminhtml_Block_System_Config_Form_Field
+class Fyndiq_Fyndiq_Block_Updated extends Mage_Adminhtml_Block_System_Config_Form_Field
 {
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
         $this->setElement($element);
 
-        $html = '<textarea readonly="readonly">'.''.'</textarea>';
+        $observer = Mage::getModel('fyndiq/observer');
+        $storeId = $observer->getStoreId();
+        $generatedTime = Mage::getModel('fyndiq/config')->get('fyndiq/feed/generated_time', $storeId);
+        $label = Mage::helper('fyndiq_fyndiq')->__('never');
+        if ($generatedTime) {
+            $label = date('r', $generatedTime);
+        }
+        $html = '<label style="font-weight: bold;">'.$label.'</label>';
         return $html;
     }
 }
