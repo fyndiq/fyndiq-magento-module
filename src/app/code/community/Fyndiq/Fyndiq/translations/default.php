05cd61d3ad0152b6f20d13d2a824954ffde328a8
aquilax <aquilax@gmail.com>
2016-03-03 11:33:38 +0100

    Added update categories button

--- src/app/code/community/Fyndiq/Fyndiq/translations/default.php
+++ src/app/code/community/Fyndiq/Fyndiq/translations/default.php
@@ -110,4 +110,7 @@ $lang = array(
     'The configuration could not be sent to Fyndiq (%s)' => 'The configuration could not be sent to Fyndiq (%s)',
     'Incorrect Username or API Token' => 'Incorrect Username or API Token',
 
+    'Fyndiq Category Mapping' => 'Fyndiq Category Mapping',
+    'Category Mapping' => 'Category Mapping',
+    'Update Fyndiq Cateogories' => 'Update Fyndiq Cateogories',
 );
