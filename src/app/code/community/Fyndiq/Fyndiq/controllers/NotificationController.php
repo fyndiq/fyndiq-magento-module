8c653c3f40ef4b6d13a05302de963834643b1c02
Håkan Nylén <hakan@fyndiq.se>
2016-03-31 08:36:20 +0200

    using 403 for enable_debug check

--- src/app/code/community/Fyndiq/Fyndiq/controllers/NotificationController.php
+++ src/app/code/community/Fyndiq/Fyndiq/controllers/NotificationController.php
@@ -196,7 +196,11 @@ class Fyndiq_Fyndiq_NotificationController extends Mage_Core_Controller_Front_Ac
         // Get the setting for enable debug
         $enableDebug = (boolean)$this->configModel->get('fyndiq/troubleshooting/enable_debug', $storeId);
 
-        if (!$this->isCorrectToken($this->getRequest()->getParam('token'), $storeId) || !$enableDebug) {
+        if(!$enableDebug) {
+            return $this->getFyndiqOutput()->showError(403, 'Forbidden', 'The request did not work.');
+        }
+
+        if (!$this->isCorrectToken($this->getRequest()->getParam('token'), $storeId)) {
             return $this->getFyndiqOutput()->showError(400, 'Bad Request', 'The request did not work.');
         }
 
